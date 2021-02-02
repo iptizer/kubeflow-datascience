b4b1e94379a3a856d70be2bebca7653e849762c4
Moritz Graf <moritz.graf@continental-corporation.com>
2021-02-02 16:18:57 +0100

    Correcting start of notebook

--- start_jupyterlab.sh
+++ start_jupyterlab.sh
@@ -8,6 +8,6 @@ if [[ -f /run/secrets/kubernetes.io/serviceaccount/namespace ]]; then
     echo "{\"namespace\": \"$(cat /run/secrets/kubernetes.io/serviceaccount/namespace)\"}" >${NB_HOME}/.config/kfp/context.json
 fi
 
-jupyter lab --notebook-dir=/home/${NB_HOME} --ip=0.0.0.0 --no-browser \
+jupyter lab --notebook-dir=${NB_HOME} --ip=0.0.0.0 --no-browser \
     --allow-root --port=8888 --LabApp.token='' --LabApp.password='' \
     --LabApp.allow_origin='*' --LabApp.base_url=${NB_PREFIX}
